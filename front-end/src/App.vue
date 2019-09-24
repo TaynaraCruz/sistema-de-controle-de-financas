b19539d5d62654ad54394b78307cbe3bba1a1eff
João Gabriel de Oliveira Bicalho <jgobi@ufmg.br>
2019-09-24 18:29:45 -0300

    base front

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,78 +1,94 @@
 <template>
-    <v-app>
-        <v-toolbar :color="`${$route.meta.color || 'green'} darken-2`" dark app>
-            <v-toolbar-title class="">
-                <h1>Housefly</h1>
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-toolbar-items>
-                <v-btn flat exact v-for="link in links" :key="link.to" :to="link.to">
-                    <v-icon class="hidden-md-and-up">{{link.icon}}</v-icon>
-                    <span class="hidden-sm-and-down">{{link.text}}</span>
-                </v-btn>
-            </v-toolbar-items>
-        </v-toolbar>
+    <v-app id="inspire">
+        <v-navigation-drawer v-model="drawer" app clipped>
+            <v-list dense>
+                <v-list-item :to="link.to" v-for="link in links" :key="link.to">
+                    <v-list-item-action>
+                        <v-icon>{{link.icon}}</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>{{link.text}}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar app clipped-left>
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title>お金師団</v-toolbar-title>
+            <div class="flex-grow-1"></div>
+            <v-btn icon @click="invertColors">
+                <v-icon>mdi-invert-colors</v-icon>
+            </v-btn>
+        </v-app-bar>
 
         <v-content>
             <transition name="fade" mode="out-in">
-                <router-view/>
+                <router-view />
             </transition>
         </v-content>
 
-        <our-footer />
+        <v-footer app absolute class="font-weight-medium">
+            <v-col class="text-center" cols="12">
+                {{ new Date().getFullYear() }} — <a class="footer-a" href="https://011235.xyz" target="_blank"><strong>お金師団</strong></a>
+            </v-col>
+        </v-footer>
     </v-app>
 </template>
 
 <script>
-    import OurFooter from './components/OurFooter';
-    export default {
-        name: 'App',
-        components: {
-            OurFooter
+export default {
+    data: () => ({
+        drawer: null,
+        links: [{
+            to: '/',
+            icon: 'mdi-home',
+            text: 'Teste',
+        }, {
+            to: '/estatisticas',
+            icon: 'mdi-chart-areaspline',
+            text: 'Estatísticas',
+        }, {
+            to: '/herois',
+            icon: 'mdi-account',
+            text: 'Heróis',
+        }, {
+            to: '/sobre',
+            icon: 'mdi-star',
+            text: 'Sobre',
+        }]
+    }),
+    methods: {
+        invertColors() {
+            this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+            window.localStorage.light = ''+!this.$vuetify.theme.dark;
         },
-        data() {
-            return {
-                links: [{
-                    to: '/',
-                    icon: 'home',
-                    text: 'Teste',
-                },{
-                    to: '/estatisticas',
-                    icon: 'timeline',
-                    text: 'Estatísticas',
-                },{
-                    to: '/herois',
-                    icon: 'person',
-                    text: 'Heróis',
-                },{
-                    to: '/sobre',
-                    icon: 'star',
-                    text: 'Sobre',
-                }]
-            }
+    },
+    created () {
+        if (window.localStorage.light === 'true') {
+            this.$vuetify.theme.dark = false;
         }
-    }
+    },
+}
 </script>
 
 <style scoped>
-    @media screen and (max-width: 960px) {
-        .v-btn {
-            min-width: 0;
-        }
-        .v-btn__content {
-            padding: 0;
-        }
-    }
     .fade-leave-active,
     .fade-enter-active {
         transition: 0.25s;
     }
+
     .fade-enter,
     .fade-leave-to {
-        transform: translateY(10vh);
+        transform: translateY(3vh);
         opacity: 0;
     }
-    .v-content {
-        background-color: #696969;
+
+    .footer-a {
+        color: inherit;
+        text-decoration: none;
+    }
+    .footer-a:hover {
+        text-decoration: underline;
     }
 </style>
--- front-end/src/plugins/vuetify.js
+++ front-end/src/plugins/vuetify.js
@@ -3,8 +3,17 @@ import Vuetify from 'vuetify/lib';
 
 Vue.use(Vuetify);
 
+import pt from 'vuetify/es5/locale/pt';
+
 export default new Vuetify({
-  icons: {
-    iconfont: 'mdi',
-  },
+    icons: {
+        iconfont: 'mdi',
+    },
+    theme: {
+        dark: true,
+    },
+    lang: {
+        locales: { pt },
+        current: 'pt',
+    },
 });
