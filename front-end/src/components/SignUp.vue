3a221c30240961d86f2bc6704675f3fb1b4f1c25
LarissaGomes <larissamalagoli@outlook.com>
2019-09-24 21:39:35 -0300

    Tela de registro de usuários adicionada

---
+++ front-end/src/components/SignUp.vue
@@ -0,0 +1,66 @@
+<template>
+<div id="app">
+  <v-app id="inspire">
+    <v-form
+      ref="form"
+      v-model="valid"
+      lazy-validation
+    >
+      <v-text-field
+        v-model="name"
+        :counter="10"
+        :rules="nameRules"
+        label="Nome"
+        required
+      ></v-text-field>
+  
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="E-mail"
+        required
+      ></v-text-field>
+  
+      <v-text-field
+        v-model="senha"
+        :rules="senha"
+        label="Senha"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="confirmar_senha"
+        :rules="confirmar_senha"
+        label="Confirmar a Senha"
+        required
+      ></v-text-field>
+  
+      <v-checkbox
+        v-model="checkbox"
+        :rules="[v => !!v || 'You must agree to continue!']"
+        label="Você concorda que nosso projeto é bonito?"
+        required
+      ></v-checkbox>
+  
+      <v-btn
+        :disabled="!valid"
+        color="success"
+        class="mr-4"
+        @click="validate"
+      >
+        Validate
+      </v-btn>
+  
+      <v-btn
+        color="error"
+        class="mr-4"
+        @click="reset"
+      >
+        Reset Form
+      </v-btn>
+  
+    
+    </v-form>
+  </v-app>
+</div>
+</template>
