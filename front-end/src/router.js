b60d44996491ee7f430d811e8d4ed775b285d493
Joao Vitor Campos Teixeira <joao.campos@araguaia.grad.dcc.ufmg.br>
2019-09-25 16:07:09 -0300

    Adicionada tela de Login e botão de Sign Up com

--- front-end/src/router.js
+++ front-end/src/router.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Login from "./views/LoginPage.vue";
 import Profile from "./views/Profile.vue";
+import SignUp from "./components/SignUp.vue"
 
 Vue.use(Router);
 
@@ -16,6 +18,21 @@ export default new Router({
       path: "/profile",
       name: "profile",
       component: Profile,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: Profile,
+    },
+    {
+      path: "/SignUp",
+      name: "Signup",
+      component: SignUp,
     },
     {
       path: "/about",
---
+++ front-end/src/views/LoginPage.vue
@@ -0,0 +1,103 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="greay darken-2"
+                dark
+                flat
+              >
+                <v-toolbar-title class="text-center">Login form</v-toolbar-title>
+                <div class="flex-grow-1"></div>
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <v-btn
+                      :href="source"
+                      icon
+                      large
+                      target="_blank"
+                      v-on="on"
+                    >
+                    </v-btn>
+                  </template>
+                  <span>Source</span>
+                </v-tooltip>
+                <v-tooltip right>
+                  <template v-slot:activator="{ on }">
+                    <v-btn
+                      icon
+                      large
+                      href="https://codepen.io/johnjleider/pen/pMvGQO"
+                      target="_blank"
+                      v-on="on"
+                    >
+                    </v-btn>
+                  </template>
+                  <span>Codepen</span>
+                </v-tooltip>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Login"
+                    name="login"
+                    prepend-icon="mdi-account"
+                    type="text"
+                  ></v-text-field>
+
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn 
+                  color="success"
+                  to="/SignUp"
+                > 
+                  SignUp
+                </v-btn>
+                <div class="flex-grow-1"></div>
+                <v-btn color="gray darken-2">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+            <v-btn
+                :href="source"
+                icon
+                large
+            >
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+    }),
+  }
+</script>
