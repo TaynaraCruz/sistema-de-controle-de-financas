adccc351c32bf41e492d91ca67cb427631f0300f
João Gabriel <joaogabrieloliveira9@gmail.com>
2019-10-18 19:25:11 -0300

    view user

---
+++ back-end/api/user/index.js
@@ -0,0 +1,14 @@
+const db = require('../../lib/db');
+const { ifAuth } = require('../../lib/auth');
+const SQL = require('sql-template-strings');
+
+module.exports = ifAuth(async (req, res) => {
+    let result = await db.query(SQL`select id, name, income, email from User where id=${req.user.id};`);
+    if (result.error) {
+        return res.status(500).json({error: 'internal server error', details: result.error.toString()});
+    } else {
+        return res.status(200).json({
+            user: result[0],
+        });
+    }
+});
