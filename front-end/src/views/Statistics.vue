96ee706b339394a6d781ea6c9fbcc2a29cf3c9d7
LarissaGomes <larissamalagoli@outlook.com>
2019-10-19 12:57:18 -0300

    Adicionado esqueleto da página de estatísticas

--- front-end/src/views/Statistics.vue
+++ front-end/src/views/Statistics.vue
@@ -6,29 +6,20 @@
         <v-flex xs12>
                 <v-img :src="require('../assets/logo.png')" class="my-3" contain height="200"></v-img>
             </v-flex>
-        Incluir Morador
+        Histórico de despesas
       </v-row>
     </h3>
     <p> </p>
-    <p class="text-center"> Para incluir um membro a casa atual é necessário fornecer o login do usuário a ser adicionado.</p>
+    <p class="text-center"> Gráficos gerados utilizando as informações disponíveis da casa em questão.</p>
     <form>
-      <v-row  justify="center"> 
-        <v-col cols="4">
-          <v-text-field
-            label="Nome do novo membro"
-            prepend-icon="mdi-account-plus"
-          ></v-text-field>
-        </v-col>
-      </v-row>
-    <v-row>
-    <v-spacer></v-spacer>
-    <v-spacer></v-spacer>
-    </v-row>
-    <v-col cols="32" sm="36" md="34">
-      <v-card-actions class="justify-center">
-        <v-btn primary>
-          Registrar
-        </v-btn>
+   
+      <v-card class="mb-3">
+            <v-card-title primary-title><h4 class="title">Divisão dos gastos do último mês:</h4></v-card-title>
+            <v-card-text>
+                <apexchart height="320" type="donut" :options="chartOptions" :series="series"></apexchart>
+            </v-card-text>
+      </v-card>
+
       </v-card-actions>
       <v-card-actions class="justify-center">
         <v-btn primary color="success">
@@ -41,3 +32,8 @@
   </v-app>
 </div>
 </template>
+
+
+<script>
+
+</script>
