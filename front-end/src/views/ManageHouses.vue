e80580093af83d17825f71ee08892144ba57d56f
Jvccampos <jok772010@gmail.com>
2019-10-19 16:25:45 -0300

    Melhorias na interface da tela de Gerenciar Casas

--- front-end/src/views/ManageHouses.vue
+++ front-end/src/views/ManageHouses.vue
@@ -9,32 +9,41 @@
           md="4"
         >
           <v-item v-slot:default="{ active, toggle }">
-            <v-card
-              :color="active ? 'primary' : ''"
-              class="display-3 flex-grow-1 text-center"
-              dark
-              height="400"
-              @click="toggle"
-            >
-            <v-card-text class="text-center title">Casa X</v-card-text>
-              <v-scroll-y-transition>
-              </v-scroll-y-transition>
-            </v-card>
-          </v-item>
-        </v-col>
+                <v-card
+                    class="mx-auto"
+                    max-width="450"
+                >
+                    <v-img
+                    src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
+                    :aspect-ratio="16/9"
+                    >
 
-        <v-col
-          cols="12"
-          md="4"
-        >
-            <v-card
-                color= 'green'
-                class="display-3 flex-grow-1 text-center"
-                dark
-                height="400"
-            >
-           <v-card-text class="text-center title">Adicionar Nova Casa</v-card-text>
-            </v-card>
+                    </v-img>
+                    <v-card-title>
+                    <div class="display-1 mb-2">Casa do Jorge ADM</div>
+                    <div class="title font-weight-regular grey--text">Uma casa da pesada,e os moradores entram em várias encrencas</div>
+                    </v-card-title>
+
+                    <v-divider class="mt-6 mx-4"></v-divider>
+
+                    <v-card-text align="center">
+                    <v-chip
+                        class="mr-3 ml"
+                        @click="lights"
+                    >
+                        <v-icon left>mdi-brightness-5</v-icon>
+                       Editar Casa
+                    </v-chip>
+                    <v-chip
+                        class="mr-3"
+                        @click="alarm"
+                    >
+                        <v-icon left>mdi-delete</v-icon>
+                        Deletar Casa
+                    </v-chip>
+                    </v-card-text>
+                </v-card>
+          </v-item>
         </v-col>
       </v-row>
     </v-container>
