10f493a83dcd5023ef2f9e404894db67aa8f6f7a
José Oliveira <josecarlosdeoliveirajr@gmail.com>
2019-10-20 20:11:51 -0300

    Back-end: Add admin to LiveIn when create house.

--- back-end/api/house/new.js
+++ back-end/api/house/new.js
@@ -1,5 +1,7 @@
 const db = require('../../lib/db');
-const { ifAuth } = require('../../lib/auth');
+const {
+    ifAuth
+} = require('../../lib/auth');
 const SQL = require('sql-template-strings');
 
 module.exports = ifAuth(async (req, res) => {
@@ -9,26 +11,36 @@ module.exports = ifAuth(async (req, res) => {
         });
     }
 
-    const { name } = req.body;
+    const {
+        name
+    } = req.body;
     if (!name) {
         return res.status(400).json({
             error: 'incomplete form'
         });
     }
 
-    const result = await db.query(SQL`INSERT INTO
-            House(name, admin_id) VALUES (
-                "${name}", ${req.user.id}
-            );`)
-    
-    if (result.error) {
-        return res.status(500).json({
+    let results = await db.transaction().query(SQL`
+        INSERT INTO
+            House(name, admin_id)
+        VALUES
+            (${name}, ${req.user.id});`
+    ).query((r) => [SQL`
+        INSERT INTO
+            LiveIn(user_id, house_id)
+        VALUES
+            (${req.user.id}, ${r.insertId});`]
+    ).rollback((e) => {
+        // TODO: Fix error here.
+        res.status(500).json({
             error: 'Internal server error',
-            details: result.error.toString()
+            details: e.toString()
         });
-    } else {
-        return res.status(200).json({
-            created: result.insertId
-        });
-    }
+    }).commit();
+
+    db.end();
+
+    return res.status(200).json({
+        created: results[0].insertId
+    })
 });
