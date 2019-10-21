83651cf18a4253b8fe1873ee303b3e8a46a7f536
Taynara Lorraine Marcelino da Cruz <taynaracruz@vermelho.grad.dcc.ufmg.br>
2019-10-21 14:14:45 -0300

    Merge branch 'master' of https://github.com/TaynaraCruz/sistema-de-controle-de-financas

--- front-end/src/views/AddHouse.vue
+++ front-end/src/views/AddHouse.vue
@@ -19,16 +19,10 @@
                 <v-row justify="center">
                     <v-col cols="4">
                         <v-text-field
+                            v-model="name"
                             label="Nome da casa"
                             prepend-icon="mdi-home"
-                        ></v-text-field>
-                    </v-col>
-                </v-row>
-                <v-row justify="center">
-                    <v-col cols="4">
-                        <v-text-field
-                            label="Descrição da casa"
-                            prepend-icon="mdi-alphabetical"
+                            required
                         ></v-text-field>
                     </v-col>
                 </v-row>
@@ -37,7 +31,7 @@
                     <v-spacer></v-spacer>
                 </v-row>
                 <v-col cols="32" sm="36" md="34">
-                    <v-card-actions class="justify-center">
+                    <v-card-actions class="justify-center" @click="addHouse">
                         <v-btn primary>
                             Registrar
                         </v-btn>
@@ -49,6 +43,37 @@
                     </v-card-actions>
                 </v-col>
             </form>
+            <p :v-if="error">{{error}}</p>
         </v-app>
     </div>
 </template>
+
+<script>
+import { getUser } from '../login';
+export default {
+    props: {
+        source: String,
+    },
+    data: () => ({
+        drawer: null,
+        name: '',
+        error: '',
+    }),
+    methods: {
+        async addHouse(){
+            try {
+                this.error = '';
+                await this.$root.$request('house/new', {
+                    name: this.name,
+                });
+                alert("Casa criada com sucesso");
+                this.$router.push('/manage');
+                
+            } catch (err) {
+                this.error = err.error;
+                console.log(err)
+            }
+        }
+    },
+};
+</script>
