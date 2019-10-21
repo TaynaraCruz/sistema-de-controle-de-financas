5dfaf0aa79627731e26821e7fae0029ed79eb65b
José Oliveira <josecarlosdeoliveirajr@gmail.com>
2019-10-21 00:57:25 -0300

    Back-end: List all expenses of a house.

--- back-end/README.md
+++ back-end/README.md
@@ -3,6 +3,7 @@
 | Route | Parameters | Return | Comentary |
 |:-|:-|:-|:-|
 | `.` | | | Print "oi". |
+| `expense` | `id`: string\|numero | `expenses`: object | Return all expenses of the house. Resdent must be authenticated. |
 | `expense/[id]` | | `expense`: object | Return expense by id. Resident of the house which the expense belongs must be authenticated. |
 | `expense/new` | `name`: string<br>`date`: date<br>`value`: number<br>`houseId`: string\|number | `created`: number | Create a new expanse. Return its id. Admin of the house must authenticated. |
 | `login` | `email`: string<br>`password`: string | `user`: object<br>`token`: string | Check email and password combination and return object with user's object and token. |
---
+++ back-end/api/expense/index.js
@@ -0,0 +1,56 @@
+const db = require('../../lib/db');
+const {
+    ifAuth
+} = require('../../lib/auth');
+const SQL = require('sql-template-strings');
+
+module.exports = ifAuth(async (req, res) => {
+    if (typeof req.body !== 'object') {
+        return res.status(400).json({
+            error: 'invalid request'
+        })
+    }
+
+    const {
+        id
+    } = req.body;
+    if (!id) {
+        return res.status(400).json({
+            error: 'incomplete form'
+        });
+    }
+
+    const checkResident = await db.query(SQL `
+        SELECT 
+            COUNT(*) as count
+        FROM
+            LiveIn
+        WHERE
+            user_id = ${+req.user.id} AND house_id = ${id};
+    `);
+    if (checkResident.error) {
+        return res.status(500).json({
+            error: 'internal server error',
+            details: result.error.toString()
+        });
+    }
+
+    const result = await db.query(SQL `
+       SELECT
+            id, name, date, value
+        FROM
+            Expense
+        WHERE
+            house_id = 1;
+    `);
+    if (result.error) {
+        return res.status(500).json({
+            error: 'Internal server error',
+            details: result.error.toString()
+        });
+    } else {
+        return res.status(200).json({
+            expenses: result
+        });
+    }
+});
